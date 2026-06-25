572124c61604fa11b478bcbe1a3d253baf00d43a
Timeo Bavart <tbavart@gmail.com>
2026-06-25 20:07:06 +0200

    remove: debug

--- src/protocol/packets/handshake.rs
+++ src/protocol/packets/handshake.rs
@@ -18,13 +18,10 @@ pub struct HandshakePacket {
 impl HandshakePacket {
     pub fn decode(reader: &mut PacketReader) -> Result<Self, &'static str> {
         let protocol_version = reader.read_varint()?;
-        println!("protocol version: {}", protocol_version);
 
         let server_address = reader.read_string()?;
-        println!("server address: {}", server_address);
 
         let server_port = reader.read_u16()?;
-        println!("server port: {}", server_port);
 
         let intent = match reader.read_varint()? {
             1 => Intent::Status,
@@ -32,7 +29,6 @@ impl HandshakePacket {
             3 => Intent::Transfer,
             _ => return Err("unknown intent"),
         };
-        println!("intent: {:?}", intent);
 
         Ok(Self {
             protocol_version,
